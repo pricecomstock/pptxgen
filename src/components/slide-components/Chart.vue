6f2a72c88a84a1a5864693461a18cc2f0e7e2eed
Price Comstock <price.comstock@gmail.com>
2019-05-13 21:09:39 -0500

    linted

--- src/components/slide-components/Chart.vue
+++ src/components/slide-components/Chart.vue
@@ -8,36 +8,35 @@
 </template>
 
 <script>
-
 import barChart from "./BarChart.vue";
 export default {
-    props: {
-        vwHeight: {
-            type: Number,
-            default: 25
-        },
-        vwWidth: {
-            type: Number,
-            default: 20
-        },
-        chart: {
-            type: Object,
-            required: true
-        }
+  props: {
+    vwHeight: {
+      type: Number,
+      default: 25
     },
-    computed: {
-        containerStyles() {
-            return {
-                position: 'relative',
-                height: `${this.vwHeight}vw`,
-                width: `${this.vwWidth}vw`
-            }
-        }
+    vwWidth: {
+      type: Number,
+      default: 20
     },
-    components: {
-        barChart
+    chart: {
+      type: Object,
+      required: true
     }
-}
+  },
+  computed: {
+    containerStyles() {
+      return {
+        position: "relative",
+        height: `${this.vwHeight}vw`,
+        width: `${this.vwWidth}vw`
+      };
+    }
+  },
+  components: {
+    barChart
+  }
+};
 </script>
 
 <style>
