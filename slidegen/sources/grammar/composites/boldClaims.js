ffdf53fc100042ffb78bf044b559ca77e03baa69
Price Comstock <price.comstock@gmail.com>
2020-01-16 12:36:25 -0500

    added bold claims

---
+++ slidegen/sources/grammar/composites/boldClaims.js
@@ -0,0 +1,28 @@
+const boldClaims = {
+  claims: [
+    "#claimSubject# is actually #claimDescription#",
+    "There is mounting evidence for #claimSubject# being #claimDescription#"
+  ],
+
+  subject: [
+    "#event.s#",
+    "#setting.s#",
+    "#animal.s#",
+    "the #animal#",
+    "#netflixGenre# films",
+    "#company#",
+    "#bodyFluid#",
+    "#jargonNoun#",
+    "#drug#",
+    "#disease#",
+    "#monster.s#",
+    "#noun#",
+    "#object#",
+    "#object.s#",
+    "#clothing#"
+  ],
+
+  claimDescription: ["made of #noun#", "make people feel #mood#"]
+};
+
+module.exports = boldClaims;
